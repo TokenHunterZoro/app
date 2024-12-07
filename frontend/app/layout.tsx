5dcb6c066d76a2b55f65de0521ae2dd32ec735cd
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-12-07 10:48:45 +0530

    ui complete

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased select-none`}
           >
             <Layout>{children}</Layout>
           </body>
--- frontend/components/sections/ticker/tiktoks.tsx
+++ frontend/components/sections/ticker/tiktoks.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Tiktoks({ ticker }: { ticker: string }) {
   return (
     <>
@@ -6,9 +8,60 @@ export default function Tiktoks({ ticker }: { ticker: string }) {
           <p className="text-2xl font-bold nouns tracking-widest text-[#F8D12E]">
             Curated Tiktoks
           </p>
-          <p className="text-md text-muted-foreground font-semibold">
+          <p className="text-md text-muted-foreground font-semibold mb-6">
             All videos where ${ticker} was mentioned/talked about
           </p>
+          <div className="grid grid-cols-4 gap-2">
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+            <Image
+              src={"https://picsum.photos/300/500"}
+              width={300}
+              height={500}
+              alt="tiktok"
+              className="rounded-xl border border-[2px] border-secondary hover:border-muted-foreground transition duration-300 ease-in-out"
+            />
+          </div>
         </div>
       </div>
     </>
